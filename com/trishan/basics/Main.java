0afc81b51c4f83c206a6c6f46e4db07102aabf04
Trishan Wagle <mailtotrishan@gmail.com>
2024-04-11 21:40:49 +0545

    feat: college april 11.

--- com/trishan/basics/Main.java
+++ com/trishan/basics/Main.java
@@ -6,6 +6,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        // Single Line Comment
+        /* Multi-line Comment */
+        /** Javadocs */
+
+        // College - Task 1
+        System.out.print("This\n");
+        System.out.print("is my first's\n");
+        System.out.print("\"Java\" program\n");
+
+        // College - Task 2
+        System.out.print("This\ncan be \"Done\"\nin one's \"Single Line\"\n");
+
+        // College - Task 3
+        System.out.println("*");
+        System.out.println("\"*\"'");
+        System.out.println("\\***\\");
+        System.out.println("\"*\"'*'\"*\"'*'");
+        System.out.println("\"*\"\"*\"\"*\"\"*\"");
+
         // Taking Inputs
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextLine());
