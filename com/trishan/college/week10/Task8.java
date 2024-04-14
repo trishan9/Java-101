4981915beae6e42963b567e47a56d4cfb6334b2a
Trishan Wagle <mailtotrishan@gmail.com>
2024-04-14 20:31:14 +0545

    feat: oop problems

---
+++ com/trishan/college/week10/Task8.java
@@ -0,0 +1,54 @@
+package com.trishan.college.week10;
+
+public class Task8 {
+//    You are building a library management system. Design an interface named "LibraryItem" with the following methods:
+//    String getTitle() to retrieve the title of the library item
+//    String getAuthor() to retrieve the author of the library item
+//    int getYear() to retrieve the publication year of the library item
+//    boolean isAvailable() to check if the library item is currently available for borrowing.
+
+    public static void main(String[] args) {
+
+    }
+}
+
+interface ILibraryItem {
+    String getTitle();
+    String getAuthor();
+    int getYear();
+    boolean isAvailable();
+}
+
+class LibraryItem implements ILibraryItem {
+    private String title;
+    private String author;
+    private int year;
+    private boolean isAvailable;
+
+    LibraryItem(String title, String author, int year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isAvailable = true;
+    }
+
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
+
+    @Override
+    public String getAuthor() {
+        return this.author;
+    }
+
+    @Override
+    public int getYear() {
+        return this.year;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return this.isAvailable;
+    }
+}
