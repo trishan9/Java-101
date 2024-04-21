a94892a23f63f6c4b62aab63ffc4a9544a3fddbc
Trishan Wagle <mailtotrishan@gmail.com>
2024-04-21 14:35:45 +0545

    feat: college stuffs

--- com/trishan/college/week2/Lecture.java
+++ com/trishan/college/week2/Lecture.java
@@ -1,5 +1,7 @@
 package com.trishan.college.week2;
 
+import java.util.Scanner;
+
 public class Lecture {
     public static void main(String[] args) {
         // 1. Write a program to check whether a variable is greater or equal to 18
@@ -29,5 +31,26 @@ public class Lecture {
 
         // 4. Write the ternary operator for question no. 1
         System.out.println(num >= 18 ? "Yes it is greater than or equal to 18!" : "No, it is not greater than or equal to 18!");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = sc.nextLine();
+
+        System.out.print("Are you over 18? ");
+        boolean status = sc.nextBoolean();
+
+        System.out.print("How many siblings do u have? ");
+        int sibling = sc.nextInt();
+
+        System.out.printf("Hello, %s\nOver 18? %b\nYou have %d num of sibling", name, status, sibling);
+
+        if(args.length > 1){
+            System.out.println("The first arg is " + args[0]);
+            System.out.println("The second arg is " + args[1]);
+            int sum = 10 + Integer.parseInt(args[1]);
+            System.out.println(sum);
+        } else {
+            System.out.println("Not enough arguments");
+        }
     }
 }
